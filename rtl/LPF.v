c50f43fa2ce8e8e13312ca2c60858f9ce031413f
brsf11 <brsf11@outlook.com>
2022-03-11 15:27:31 +0800

    add lpf

--- rtl/LPF.v
+++ rtl/LPF.v
@@ -1,18 +1,15 @@
-module LPF_Filter( input wire clk,
-input wire clk_enable,
-input wire reset,
+module LPF_Filter(  input wire        clk,
+                    input wire        clk_enable,
+                    input wire        reset_n,
+                    input wire signed [9:0]  filter_din,
+                    output wire signed [9:0] filter_dout);
 
-input wire[31:0] DinTick,
-output wire[31:0] OutTick,
+wire signed [19:0] filter_in;
+wire signed [19:0] filter_out;
 
-input wire [9:0] filter_din,
-output wire [9:0] filter_dout);
-wire [31:0] filter_in;
-wire [31:0] filter_out;
+assign filter_in = filter_din[9]?({10'b11111_11111,filter_din[9:0]}):({10'b0,filter_din[9:0]});
 
-assign filter_in = filter_din[9]?({filter_din[9],22'b11_11111_11111_11111_11111,filter_din[8:0]}):({filter_din[9],22'd0,filter_din[8:0]});
-
-assign filter_dout = filter_out[31]?({filter_out[31],filter_out[24:16]}):({filter_out[31],filter_out[24:16]});
+assign filter_dout = filter_out[18:9];
 
  
 
@@ -25,107 +22,92 @@ assign filter_dout = filter_out[31]?({filter_out[31],filter_out[24:16]}):({filte
 // Type Definitions
 // Constants
 
-parameter coeff1 = 32'd530; // 0.00808453 8.0845306443242445E-003; //double
-parameter coeff2 = 32'd1418; // 0.02164117 2.1641173173083540E-002; //double
-parameter coeff3 = -32'd210; //-0.00320142 -3.2014226698098332E-003; //double
-parameter coeff4 = -32'd677; //-0.01032774 -1.0327740008324267E-002; //double
-parameter coeff5 = 32'd1386; // 0.02114663 2.1146841746630163E-002; //double
-parameter coeff6 = -32'd1453; //-0.02217336 -2.2173359666555145E-002; //double
-parameter coeff7 = 32'd427;// 0.00651070 6.5107025473657917E-003; //double
-parameter coeff8 = 32'd1617;//0.0246716864 2.4671686418581144E-002; //double
-parameter coeff9 = -32'd3849;//-0.05873706263 -5.8373706263461472E-002; //double
-parameter coeff10 = 32'd4683;//0.071458229149 7.1458229149308564E-002; //double
-parameter coeff11 = -32'd2339;//-0.035685782197 -3.5685782197239432E-002; //double
-parameter coeff12 = -32'd5937;//-0.0905934127344 -9.0593412734435275E-002; //double
-parameter coeff13 = 32'd38766;//0.591529450517 5.9152945051788575E-001; //double
-parameter coeff14 = 32'd38766;//0.591529450517 5.9152945051788575E-001; //double
-parameter coeff15 = -32'd5937;//-0.090593412733 -9.0593412734435275E-002; //double
-parameter coeff16 = -32'd2339;//-0.035685782197 -3.5685782197239432E-002; //double
-parameter coeff17 = 32'd4683;//0.0714582291493 7.1458229149308564E-002; //double
-parameter coeff18 = -32'd3849;//-0.05873706263 -5.8373706263461472E-002; //double
-parameter coeff19 = 32'd1617;//0.02467168641858 2.4671686418581144E-002; //double
-parameter coeff20 = 32'd427;//0.006510702547 6.5107025473657917E-003; //double
-parameter coeff21 = -32'd1453;//-0.022173359666 -2.2173359666555145E-002; //double
-parameter coeff22 = 32'd1386;//0.0211468417466 2.1146841746630163E-002; //double
-parameter coeff23 = -32'd677;//-0.01032774 -1.0327740008324267E-002; //double
-parameter coeff24 = -32'd210;//-0.0032014226698 -3.2014226698098332E-003; //double
-parameter coeff25 = 32'd1418;//0.021641173173 2.1641173173083540E-002; //double
-parameter coeff26 = 32'd530;//0.0080084530644 8.0845306443242445E-003; //double
+parameter signed coeff1  = 6'd0;
+parameter signed coeff2  = 6'd1;
+parameter signed coeff3  = 6'd1;
+parameter signed coeff4  = 6'd2;
+parameter signed coeff5  = 6'd3;
+parameter signed coeff6  = 6'd5;
+parameter signed coeff7  = 6'd7;
+parameter signed coeff8  = 6'd9;
+parameter signed coeff9  = 6'd11;
+parameter signed coeff10 = 6'd13;
+parameter signed coeff11 = 6'd16;
+parameter signed coeff12 = 6'd18;
+parameter signed coeff13 = 6'd20;
+parameter signed coeff14 = 6'd22;
+parameter signed coeff15 = 6'd23;
+parameter signed coeff16 = 6'd24;
+parameter signed coeff17 = 6'd24;
+parameter signed coeff18 = 6'd23;
+parameter signed coeff19 = 6'd22;
+parameter signed coeff20 = 6'd20;
+parameter signed coeff21 = 6'd18;
+parameter signed coeff22 = 6'd16;
+parameter signed coeff23 = 6'd13;
+parameter signed coeff24 = 6'd11;
+parameter signed coeff25 = 6'd9;
+parameter signed coeff26 = 6'd7;
+parameter signed coeff27 = 6'd5;
+parameter signed coeff28 = 6'd3;
+parameter signed coeff29 = 6'd2;
+parameter signed coeff30 = 6'd1;
+parameter signed coeff31 = 6'd1;
+parameter signed coeff32 = 6'd0;
 
  
 
 
 // Signals
-reg[31:0] delay_pipeline [0:25] ; // double
-reg[31:0] product26; // double
-reg[31:0] product25; // double
-reg[31:0] product24; // double
-reg[31:0] product23; // double
-reg[31:0] product22; // double
-reg[31:0] product21; // double
-reg[31:0] product20; // double
-reg[31:0] product19; // double
-reg[31:0] product18; // double
-reg[31:0] product17; // double
-reg[31:0] product16; // double
-reg[31:0] product15; // double
-reg[31:0] product14; // double
-reg[31:0] product13; // double
-reg[31:0] product12; // double
-reg[31:0] product11; // double
-reg[31:0] product10; // double
-reg[31:0] product9; // double
-reg[31:0] product8; // double
-reg[31:0] product7; // double
-reg[31:0] product6; // double
-reg[31:0] product5; // double
-reg[31:0] product4; // double
-reg[31:0] product3; // double
-reg[31:0] product2; // double
-reg[31:0] product1_cast; // double
-reg[31:0] product1; // double
-reg[31:0] sum1; // double
-reg[31:0] sum2; // double
-reg[31:0] sum3; // double
-reg[31:0] sum4; // double
-reg[31:0] sum5; // double
-reg[31:0] sum6; // double
-reg[31:0] sum7; // double
-reg[31:0] sum8; // double
-reg[31:0] sum9; // double
-reg[31:0] sum10; // double
-reg[31:0] sum11; // double
-reg[31:0] sum12; // double
-reg[31:0] sum13; // double
-reg[31:0] sum14; // double
-reg[31:0] sum15; // double
-reg[31:0] sum16; // double
-reg[31:0] sum17; // double
-reg[31:0] sum18; // double
-reg[31:0] sum19; // double
-reg[31:0] sum20; // double
-reg[31:0] sum21; // double
-reg[31:0] sum22; // double
-reg[31:0] sum23; // double
-reg[31:0] sum24; // double
-reg[31:0] sum25; // double
-reg[31:0] output_typeconvert; // double
-reg[31:0] output_register; // double
+reg signed [19:0] delay_pipeline [0:31] ;
+reg signed [19:0] product32; 
+reg signed [19:0] product31; 
+reg signed [19:0] product30; 
+reg signed [19:0] product29; 
+reg signed [19:0] product28; 
+reg signed [19:0] product27;
+reg signed [19:0] product26; 
+reg signed [19:0] product25; 
+reg signed [19:0] product24; 
+reg signed [19:0] product23; 
+reg signed [19:0] product22; 
+reg signed [19:0] product21; 
+reg signed [19:0] product20; 
+reg signed [19:0] product19; 
+reg signed [19:0] product18; 
+reg signed [19:0] product17; 
+reg signed [19:0] product16; 
+reg signed [19:0] product15; 
+reg signed [19:0] product14; 
+reg signed [19:0] product13; 
+reg signed [19:0] product12; 
+reg signed [19:0] product11; 
+reg signed [19:0] product10; 
+reg signed [19:0] product9; 
+reg signed [19:0] product8; 
+reg signed [19:0] product7; 
+reg signed [19:0] product6; 
+reg signed [19:0] product5; 
+reg signed [19:0] product4; 
+reg signed [19:0] product3; 
+reg signed [19:0] product2; 
+reg signed [19:0] product1; 
+
 
 // Block Statements
-always @( posedge clk or posedge reset)
+always @( posedge clk or negedge reset_n)
 begin: Delay_Pipeline_process
-if (reset == 1'b1) begin
-delay_pipeline[0] <= 0;
-delay_pipeline[1] <= 0;
-delay_pipeline[2] <= 0;
-delay_pipeline[3] <= 0;
-delay_pipeline[4] <= 0;
-delay_pipeline[5] <= 0;
-delay_pipeline[6] <= 0;
-delay_pipeline[7] <= 0;
-delay_pipeline[8] <= 0;
-delay_pipeline[9] <= 0;
+if (reset_n == 0) begin
+delay_pipeline[0]  <= 0;
+delay_pipeline[1]  <= 0;
+delay_pipeline[2]  <= 0;
+delay_pipeline[3]  <= 0;
+delay_pipeline[4]  <= 0;
+delay_pipeline[5]  <= 0;
+delay_pipeline[6]  <= 0;
+delay_pipeline[7]  <= 0;
+delay_pipeline[8]  <= 0;
+delay_pipeline[9]  <= 0;
 delay_pipeline[10] <= 0;
 delay_pipeline[11] <= 0;
 delay_pipeline[12] <= 0;
@@ -142,20 +124,26 @@ delay_pipeline[22] <= 0;
 delay_pipeline[23] <= 0;
 delay_pipeline[24] <= 0;
 delay_pipeline[25] <= 0;
+delay_pipeline[26] <= 0;
+delay_pipeline[27] <= 0;
+delay_pipeline[28] <= 0;
+delay_pipeline[29] <= 0;
+delay_pipeline[30] <= 0;
+delay_pipeline[31] <= 0;
 end
 else begin
 if (clk_enable == 1'b1) begin
-delay_pipeline[0] <= filter_in;
-delay_pipeline[1] <= delay_pipeline[0];
-delay_pipeline[2] <= delay_pipeline[1];
-delay_pipeline[3] <= delay_pipeline[2];
-delay_pipeline[4] <= delay_pipeline[3];
-delay_pipeline[5] <= delay_pipeline[4];
-delay_pipeline[6] <= delay_pipeline[5];
-delay_pipeline[7] <= delay_pipeline[6];
-delay_pipeline[8] <= delay_pipeline[7];
-delay_pipeline[9] <= delay_pipeline[8];
-delay_pipeline[10] <= delay_pipeline[9];
+delay_pipeline[0]  <= filter_in;
+delay_pipeline[1]  <= delay_pipeline[0] ;
+delay_pipeline[2]  <= delay_pipeline[1] ;
+delay_pipeline[3]  <= delay_pipeline[2] ;
+delay_pipeline[4]  <= delay_pipeline[3] ;
+delay_pipeline[5]  <= delay_pipeline[4] ;
+delay_pipeline[6]  <= delay_pipeline[5] ;
+delay_pipeline[7]  <= delay_pipeline[6] ;
+delay_pipeline[8]  <= delay_pipeline[7] ;
+delay_pipeline[9]  <= delay_pipeline[8] ;
+delay_pipeline[10] <= delay_pipeline[9] ;
 delay_pipeline[11] <= delay_pipeline[10];
 delay_pipeline[12] <= delay_pipeline[11];
 delay_pipeline[13] <= delay_pipeline[12];
@@ -171,6 +159,12 @@ delay_pipeline[22] <= delay_pipeline[21];
 delay_pipeline[23] <= delay_pipeline[22];
 delay_pipeline[24] <= delay_pipeline[23];
 delay_pipeline[25] <= delay_pipeline[24];
+delay_pipeline[26] <= delay_pipeline[25];
+delay_pipeline[27] <= delay_pipeline[26];
+delay_pipeline[28] <= delay_pipeline[27];
+delay_pipeline[29] <= delay_pipeline[28];
+delay_pipeline[30] <= delay_pipeline[29];
+delay_pipeline[31] <= delay_pipeline[30];
 end
 end
 end // Delay_Pipeline_process
@@ -178,136 +172,61 @@ end // Delay_Pipeline_process
 
 
 
-
+always @* product32 <= delay_pipeline[31] * coeff32;
+always @* product31 <= delay_pipeline[30] * coeff31;
+always @* product30 <= delay_pipeline[29] * coeff30;
+always @* product29 <= delay_pipeline[28] * coeff29;
+always @* product28 <= delay_pipeline[27] * coeff28;
+always @* product27 <= delay_pipeline[26] * coeff27;
 always @* product26 <= delay_pipeline[25] * coeff26;
-
 always @* product25 <= delay_pipeline[24] * coeff25;
-
 always @* product24 <= delay_pipeline[23] * coeff24;
-
 always @* product23 <= delay_pipeline[22] * coeff23;
-
 always @* product22 <= delay_pipeline[21] * coeff22;
-
 always @* product21 <= delay_pipeline[20] * coeff21;
-
 always @* product20 <= delay_pipeline[19] * coeff20;
-
 always @* product19 <= delay_pipeline[18] * coeff19;
-
 always @* product18 <= delay_pipeline[17] * coeff18;
-
 always @* product17 <= delay_pipeline[16] * coeff17;
-
 always @* product16 <= delay_pipeline[15] * coeff16;
-
 always @* product15 <= delay_pipeline[14] * coeff15;
-
 always @* product14 <= delay_pipeline[13] * coeff14;
-
 always @* product13 <= delay_pipeline[12] * coeff13;
-
 always @* product12 <= delay_pipeline[11] * coeff12;
-
 always @* product11 <= delay_pipeline[10] * coeff11;
-
-always @* product10 <= delay_pipeline[9] * coeff10;
-
-always @* product9 <= delay_pipeline[8] * coeff9;
-
-always @* product8 <= delay_pipeline[7] * coeff8;
-
-always @* product7 <= delay_pipeline[6] * coeff7;
-
-always @* product6 <= delay_pipeline[5] * coeff6;
-
-always @* product5 <= delay_pipeline[4] * coeff5;
-
-always @* product4 <= delay_pipeline[3] * coeff4;
-
-always @* product3 <= delay_pipeline[2] * coeff3;
-
-always @* product2 <= delay_pipeline[1] * coeff2;
-
-always @* product1_cast <= product1;
-
-always @* product1 <= delay_pipeline[0] * coeff1;
+always @* product10 <= delay_pipeline[9]  * coeff10;
+always @* product9  <= delay_pipeline[8]  * coeff9;
+always @* product8  <= delay_pipeline[7]  * coeff8;
+always @* product7  <= delay_pipeline[6]  * coeff7;
+always @* product6  <= delay_pipeline[5]  * coeff6;
+always @* product5  <= delay_pipeline[4]  * coeff5;
+always @* product4  <= delay_pipeline[3]  * coeff4;
+always @* product3  <= delay_pipeline[2]  * coeff3;
+always @* product2  <= delay_pipeline[1]  * coeff2;
+always @* product1  <= delay_pipeline[0]  * coeff1;
 
  
 
 
-
-always @* sum1 <= product1_cast + product2;
-
-always @* sum2 <= sum1 + product3;
-
-always @* sum3 <= sum2 + product4;
-
-always @* sum4 <= sum3 + product5;
-
-always @* sum5 <= sum4 + product6;
-
-always @* sum6 <= sum5 + product7;
-
-always @* sum7 <= sum6 + product8;
-
-always @* sum8 <= sum7 + product9;
-
-always @* sum9 <= sum8 + product10;
-
-always @* sum10 <= sum9 + product11;
-
-always @* sum11 <= sum10 + product12;
-
-always @* sum12 <= sum11 + product13;
-
-always @* sum13 <= sum12 + product14;
-
-always @* sum14 <= sum13 + product15;
-
-always @* sum15 <= sum14 + product16;
-
-always @* sum16 <= sum15 + product17;
-
-always @* sum17 <= sum16 + product18;
-
-always @* sum18 <= sum17 + product19;
-
-always @* sum19 <= sum18 + product20;
-
-always @* sum20 <= sum19 + product21;
-
-always @* sum21 <= sum20 + product22;
-
-always @* sum22 <= sum21 + product23;
-
-always @* sum23 <= sum22 + product24;
-
-always @* sum24 <= sum23 + product25;
-
-always @* sum25 <= sum24 + product26;
-
-always @* output_typeconvert <= sum25;
-
  
 
-wire [31:0] tmp0;
-wire [31:0] tmp1;
-wire [31:0] tmp2;
-wire [31:0] tmp3;
-wire [31:0] tmp4;
-wire [31:0] tmp;
+wire signed [19:0] tmp0;
+wire signed [19:0] tmp1;
+wire signed [19:0] tmp2;
+wire signed [19:0] tmp3;
+wire signed [19:0] tmp4;
+wire signed [19:0] tmp5;
+wire signed [19:0] tmp;
 assign tmp0 = product1 + product2 + product3 + product4 + product5;
 assign tmp1 = product6 + product7 + product8 + product9 + product10;
 assign tmp2 = product11 + product12 + product13 + product14 + product15;
 assign tmp3 = product16 + product17 + product18 + product19 + product20;
 assign tmp4 = product21 + product22 + product23 + product24 + product25;
-assign tmp = tmp0 + tmp1 + tmp2 + tmp3 + tmp4 + product26;
-assign OutTick = tmp;
+assign tmp5 = product26 + product27 + product28 + product29 + product30;
+assign tmp = tmp0 + tmp1 + tmp2 + tmp3 + tmp4 + tmp5 +product31 + product32;
 
 
 
 assign filter_out = tmp;
-//assign filter_out = output_typeconvert;
 
 endmodule
--- rtl/differentiator01.v
+++ rtl/differentiator01.v
@@ -8,7 +8,7 @@ module differentiator(
 );
 reg signed [9:0] XI0,XI1,XQ0,XQ1;
 wire signed [19:0] temp1,temp2,temp3;
-wire signed [9:0] temp4;
+reg lpf_clk;
 
 always @(posedge clk or negedge rstn) begin
     if(!rstn)begin
@@ -16,6 +16,7 @@ always @(posedge clk or negedge rstn) begin
         XQ0 <= 10'b0;
         XI1 <= 10'b0;
         XQ1 <= 10'b0;
+        lpf_clk <= 0;
     end 
     else if(en) begin
 
@@ -29,6 +30,7 @@ always @(posedge clk or negedge rstn) begin
         begin
             XQ1 <= XQ0;
             XQ0 <= X - 10'd512;
+            lpf_clk <= ~lpf_clk;
         end
 
     end
@@ -39,7 +41,16 @@ end
 assign temp1 = XI0*XQ1;
 assign temp2 = XQ0*XI1;
 assign temp3 = temp2 - temp1;
-assign out = temp3[19:10] + 10'd512;
+
+wire signed [9:0] temp_lpf_out;
+assign out = (temp_lpf_out <<< 1) + 10'd512;
+
+
+LPF_Filter u_lpf (  .clk(lpf_clk),
+                    .clk_enable(1'b1),
+                    .reset_n(rstn),
+                    .filter_din(temp3[11:2]),
+                    .filter_dout(temp_lpf_out));
 
 
 
